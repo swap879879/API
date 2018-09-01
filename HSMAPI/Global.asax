fe5cf564c463d6b5f471549ec8b62be6ba164545
Sweet Bro & Sis <Sweet Bro & Sis@PRAJVAL-PC>
2018-09-01 12:58:34 +0530

    Add project files.

---
+++ HSMAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HSMAPI.MvcApplication" Language="C#" %>
---
+++ HSMAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HSMAPI.Startup))]
+namespace HSMAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
